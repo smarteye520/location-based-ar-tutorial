0792b398fdbda488caa3c9b2249ca880e82f3490
Smart Eye <smarteye0701@gmail.com>
2022-04-18 17:48:30 -0400

    Added AR Objects with Javascript.

---
+++ script.js
@@ -0,0 +1,94 @@
+
+window.onload = () => {
+    const button = document.querySelector('button[data-action="change"]');
+    button.innerText = '﹖';
+
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Pokemon1',
+            location: {
+                lat: 27.338640,
+                lng: -82.542950,
+            },
+        },
+
+        {
+            name: 'Pokemon1',
+            location: {
+                lat: 27.332479,
+                lng: -82.531929,
+            },
+        },
+    ];
+}
+
+var models = [
+    {
+        url: './assets/magnemite/scene.gltf',
+        scale: '0.3 0.3 0.3',
+        info: 'Magnemite',
+        rotation: '0 180 0',
+    },
+    {
+        url: './assets/articuno/scene.gltf',
+        scale: '0.15 0.15 0.15',
+        rotation: '0 180 0',
+        info: 'Articuno',
+    },
+    {
+        url: './assets/dragonite/scene.gltf',
+        scale: '0.06 0.06 0.06',
+        rotation: '0 180 0',
+        info: 'Dragonite',
+    },
+];
+
+var modelIndex = 0;
+var setModel = function (model, entity) {
+    if (model.scale) {
+        entity.setAttribute('scale', model.scale);
+    }
+
+    if (model.rotation) {
+        entity.setAttribute('rotation', model.rotation);
+    }
+
+    if (model.position) {
+        entity.setAttribute('position', model.position);
+    }
+
+    entity.setAttribute('gltf-model', model.url);
+
+    const div = document.querySelector('.instructions');
+    div.innerText = model.info;
+};
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+
+        setModel(models[modelIndex], model);
+
+        model.setAttribute('animation-mixer', '');
+
+        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+            var entity = document.querySelector('[gps-entity-place]');
+            modelIndex++;
+            var newIndex = modelIndex % models.length;
+            setModel(models[newIndex], entity);
+        });
+
+        scene.appendChild(model);
+    });
+}
